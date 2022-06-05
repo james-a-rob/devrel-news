a7534ec25563d9c8ae33599fc242ee2f294900f7
james <jamesrobertson@outlook.com>
2022-06-05 15:36:36 +0100

    scrape correct content

--- app.test.ts
+++ app.test.ts
@@ -1,19 +1,56 @@
-import { expect, test } from '@jest/globals';
-import { scrapeLatestStories, hnFilter } from './app';
+import { jest, describe, expect, test } from '@jest/globals';
+import nock from 'nock';
 
-test.only('scrape latest 20 articles', async () => {
+const nockBack = nock.back;
+nockBack.fixtures = '/fixtures/';
+nockBack.setMode('record');
+
+import { scrapeLatestStories, hnFilter } from './app';
+jest.setTimeout(60000);
+
+test('scrape stories', async () => {
+    const { nockDone } = await nockBack('hn-response.json')
+
     const latestStories = await scrapeLatestStories();
+    nockDone()
+
     console.log(latestStories);
 });
 
-test('filter hn story', () => {
-    const fakeHnStory = [{
-        created_at: '2022-06-03T16:09:09.000Z',
-        title: 'Show HN: Streamlit with native multipage app functionality',
-        url: 'https://share.streamlit.io/streamlit/docs/main/python/api-examples-source/mpa-hello/Hello.py',
-        author: 'TCR19',
-        points: 3,
-        num_comments: 1,
-    }];
-    expect(hnFilter(fakeHnStory)).toEqual(null);
+describe('hn filter', () => {
+    test('has enough points', () => {
+        const fakeHnStory = [{
+            created_at: '2022-06-03T16:09:09.000Z',
+            title: 'Show HN: Streamlit with native multipage app functionality',
+            url: 'https://share.streamlit.io/streamlit/docs/main/python/api-examples-source/mpa-hello/Hello.py',
+            author: 'TCR19',
+            points: 3,
+            num_comments: 1,
+        }];
+        expect(hnFilter(fakeHnStory, "github")).toEqual([]);
+    });
+
+    test('is genuine story', () => {
+        const fakeHnStory = [{
+            created_at: '2022-06-03T16:09:09.000Z',
+            title: 'Show HN: cool thing about developers',
+            url: 'https://thing.com',
+            author: 'TCR19',
+            points: 5,
+            num_comments: 1,
+        }];
+        expect(hnFilter(fakeHnStory, "github")).toEqual([]);
+    });
+
+    test('query only in title', () => {
+        const fakeHnStory = [{
+            created_at: '2022-06-03T16:09:09.000Z',
+            title: 'Cool devrel story about blah',
+            url: 'github.com/devrelnews',
+            author: 'TCR19',
+            points: 5,
+            num_comments: 1,
+        }];
+        expect(hnFilter(fakeHnStory, "github")).toEqual([]);
+    });
 });
--- app.ts
+++ app.ts
@@ -2,45 +2,74 @@ import axios from 'axios';
 interface Story {
     title: string
     url: string
+    created_at: string
+
 }
 
 interface HNStory {
     title: string
-    url: string
+    url: string | null
     points: number,
     created_at: string
 }
 
-const yesterday = (Date.now() / 1000) - (60 * 60 * 24);
-console.log(yesterday);
-export const hnFilter = (stories: HNStory[]): HNStory[] => {
-    // no show or tell
-    // ignore query mentions in link
-    // greater than 3 points
+const yesterday = (Date.now() / 1000) - ((60 * 60 * 24) * 20);
+
+export const hnFilter = (stories: HNStory[], searchTerm: string): HNStory[] => {
+    const hasEnoughPoints = (story: HNStory) => {
+        return story.points > 4;
+    }
+    const isStory = (story: HNStory) => {
+        return !story.title.includes('Show HN') && !story.title.includes('Tell HN') && !story.title.includes('Ask HN');
+
+    }
+    const queryOnlyInTitle = (story: HNStory) => {
+        return story.title.toLowerCase().includes(searchTerm);
+    }
     return stories.filter((story) => {
-        if (story.points > 5) {
-            return true;
-        } else {
-            return false;
-        }
+        return hasEnoughPoints(story) && isStory(story) && queryOnlyInTitle(story);
     });
 }
 export const scrapeLatestStories = async (): Promise<Story[]> => {
     let hnResponses: HNStory[][] = [];
-    const searchTerms = ["api", "open source"];
+    const searchTerms = [
+        "api",
+        "sdk",
+        "cli",
+        "readme",
+        "devrel",
+        "developer advocate",
+        "dev tool",
+        "ide",
+        "text editor",
+        "vs code",
+        "developer relations",
+        "devex",
+        "developer experiance",
+        "plugin",
+        "extention",
+        "twilio",
+        "stripe",
+        "keynote",
+        "open source",
+        "git",
+        "github",
+        "documentation",
+        "developers",
+    ];
     try {
         for (const searchTerm of searchTerms) {
             const hnResponse = await axios.get(`https://hn.algolia.com/api/v1/search_by_date?query="${searchTerm}"&numericFilters=created_at_i>${yesterday}&tags=story`);
             // console.log(hnResponse.data.hits);
-            const validStories = hnFilter(hnResponse.data.hits);
-            console.log(validStories);
+            const validStories = hnFilter(hnResponse.data.hits, searchTerm);
+            // console.log(validStories);
             hnResponses.push(validStories);
-
+            await new Promise(r => setTimeout(r, 1000));
         }
     } catch (e) {
         console.log(e);
         console.log("fetch failed");
         return [];
     }
-    return hnResponses.flat().map((story: HNStory) => ({ title: story.title, url: story.url, created_at: story.created_at }))
+    return hnResponses.flat().map((story: HNStory) => ({ title: story.title, url: story.url!, created_at: story.created_at }))
 }
